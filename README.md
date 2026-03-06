2fac08ee3baa321922b945527d4d2982d9c7e6ee
Luke Neale <lukeneale8@gmail.com>
2026-03-06 11:31:29 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SimStrategist
+
+Parsing code for the F1 games taken from https://github.com/Harmitx7/F1-TELEMETRY-DASHBOARD by Harmitx7
+JSON Module for LMU taken from https://community.lemansultimate.com/index.php?threads/telemetry-socket-%E2%80%93-json-telemetry-plugin.8229/
+
+To get started with a UDP listener...
+
+python3 server.py
+This will start a UDP listener on 20777
---
+++ app.py
@@ -0,0 +1,31 @@
+# Import requirements
+import os
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+import f1/config.py
+import f1/server.py
+import f1/telemetry_state.py
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///strategist.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+
